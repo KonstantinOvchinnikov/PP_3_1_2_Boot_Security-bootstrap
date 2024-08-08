0f862f5e18796b3125bbd954b1d8a0c6cadef162
KonstantinOvchinnikov <95876819+KonstantinOvchinnikov@users.noreply.github.com>
2024-08-08 16:07:51 +0300

    version beta bootstrap plus init

--- src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/kata/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,12 +2,58 @@ package ru.kata.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserServiceImp;
+import ru.kata.spring.boot_security.demo.util.MyValidator;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringBootSecurityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+        initApplicationDefault(context);
+    }
+
+    public static void initApplicationDefault(ConfigurableApplicationContext context) {
+        UserServiceImp userService = context.getBean(UserServiceImp.class);
+        RoleService roleService = context.getBean(RoleService.class);
+        MyValidator myValidator = context.getBean(MyValidator.class);
+
+        roleService.saveIfNotExist("USER");
+        roleService.saveIfNotExist("ADMIN");
+
+        User user = new User();
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleService.findByRole("USER"));
+        user.setName("User");
+        user.setSurname("User");
+        user.setAge(10);
+        user.setEmail("user@mail.ru");
+        user.setPassword("user");
+        user.setRoles(roles);
+        if (!myValidator.userInData(user.getEmail())) {
+            userService.saveUser(user);
+        }
+
+        User admin = new User();
+        admin.setName("Admin");
+        admin.setSurname("Admin");
+        admin.setAge(100);
+        admin.setEmail("admin@mail.ru");
+        admin.setPassword("admin");
+        admin.setRoles(roleService.findAll());
+        if (!myValidator.userInData(admin.getEmail())) {
+            userService.saveUser(admin);
+        }
+
+
+
+    }
 }
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -0,0 +1,42 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Service
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Transactional
+    public void saveIfNotExist(String role) {
+        Role myRole = roleRepository.findByRole(role);
+        if (myRole == null) {
+            roleRepository.save(new Role(role));
+        }
+    }
+
+    @Transactional
+    public Set<Role> findAll() {
+        return new HashSet<>(roleRepository.findAll());
+    }
+
+    @Transactional
+    public Role findByRole(String role) {
+        return roleRepository.findByRole(role);
+    }
+}
